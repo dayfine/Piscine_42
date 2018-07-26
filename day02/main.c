71e9b388b0d90a80f2d2078177d8a594423b2a51
difan <difancpa@gmail.com>
2018-07-25 17:39:17 -0700

    adding main for testing

---
+++ day02/main.c
@@ -0,0 +1,6 @@
+#include "./ft_putchar.c"
+#include "./ex00/ft_print_alphabet.c"
+#include "./ex01/ft_print_reverse_alphabet.c"
+#include "./ex02/ft_print_numbers.c"
+#include "./ex03/ft_print_comb0000000000000000000000000000000.c"
+ß
