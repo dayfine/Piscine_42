ef746cc4aca44bd563c873c6aa6d83bec5c6cf89
difan <difancpa@gmail.com>
2018-07-27 08:28:13 -0700

    day 03 file structure

---
+++ day03/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "./ft_putchar.c"
+#include "./ex00/ft_ft.c"
+#include "./ex01/ft_ultimate_ft.c"
+#include "./ex02/ft_swap.c"
+#include "./ex03/ft_div_mod.c"
+#include "./ex04/ft_ultimate_div_mod.c"
+#include "./ex05/ft_putstr.c"
+#include "./ex06/ft_strlen.c"
+#include "./ex07/ft_strrev.c"
+#include "./ex08/ft_atoi.c"
+#include "./ex09/ft_sort_integer_table.c"
+
+
+int   main(void)
+{
+	return (0);
+}
