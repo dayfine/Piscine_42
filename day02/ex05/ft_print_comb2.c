68fd20c97ae1024644ad5bb1d87ac3641f12d83d
dayfine <difancpa@gmail.com>
2018-07-26 18:17:27 -0700

    ex05

--- day02/ex05/ft_print_comb2.c
+++ day02/ex05/ft_print_comb2.c
@@ -10,7 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void  ft_print_comb2(void)
+void    ft_print_comb2_print_util(int result1[2], int result2[])
 {
-  int partial[3];
+    ft_putchar(result1[0]);
+    ft_putchar(result1[1]);
+    ft_putchar(' ');
+    ft_putchar(result2[0]);
+    ft_putchar(result2[1]);
+
+    if (!(result1[0] == '9' && result1[1] == '8'
+        && result2[0] == '9' && result2[1] == '9'))
+    {
+        ft_putchar(',');
+        ft_putchar(' ');
+    }
+}
+
+void	ft_print_comb2(void)
+{
+	int partial1[2];
+	int partial2[2];
+
+	partial1[0] = '0';
+	while (partial1[0] <= '9')
+	{
+		partial1[1] = '0';
+		while (partial1[1] <= '9')
+		{
+            partial2[0] = partial1[0];
+            while (partial2[0] <= '9')
+            {
+                partial2[1] = partial1[1];
+                while (partial2[1] <= '9')
+                {
+                    if (!(partial1[0] == partial2[0] && partial1[1] == partial2[1]))
+                    {
+                        ft_print_comb2_print_util(partial1, partial2);
+                    }
+                    partial2[1]++;
+                }
+                partial2[0]++;
+            }
+			partial1[1]++;
+		}
+		partial1[0]++;
+	}
 }
