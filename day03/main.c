29a01b0ae15095dfc68d43b68cd13b6378f870cc
difan <difancpa@gmail.com>
2018-07-27 09:09:14 -0700

    00 & 01

--- day03/main.c
+++ day03/main.c
@@ -15,5 +15,36 @@
 
 int   main(void)
 {
+	printf("%s\n", "==== ft_ft ====");
+	int num_ex01 = 12;
+
+	ft_ft(&num_ex01);
+	printf("%d\n", num_ex01);
+
+	printf("%s\n", "==== ft_ultimate_ft ====");
+	int num_ex02 = 2;
+	int *ptr1;
+	int **ptr2;
+	int ***ptr3;
+	int ****ptr4;
+	int *****ptr5;
+	int ******ptr6;
+	int *******ptr7;
+	int ********ptr8;
+	int *********ptr9;
+
+	ptr1 = &num_ex02;
+	ptr2 = &ptr1;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+
+	ft_ultimate_ft(ptr9);
+	printf("%d\n", num_ex02);
+
 	return (0);
 }
