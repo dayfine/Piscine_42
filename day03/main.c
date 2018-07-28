eda4c43e854fd9c5310339f186de6a7cc9778e33
dayfine <difancpa@gmail.com>
2018-07-27 20:21:41 -0700

    use assertion for day03

--- day03/main.c
+++ day03/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "./ft_putchar.c"
 #include "./ex00/ft_ft.c"
@@ -16,13 +17,19 @@
 int   main(void)
 {
 	printf("%s\n", "==== 00 ft_ft ====");
-	int num_ex01 = 12;
+	printf("Tesing ex00\n");
 
+	int num_ex01 = 12;
 	ft_ft(&num_ex01);
-	printf("%d\n", num_ex01);
+
+	assert(num_ex01==42);
+
+	printf("All tests passed for ex00\n");
 
 
 	printf("%s\n", "==== 01 ft_ultimate_ft ====");
+	printf("Tesing ex01\n");
+
 	int num_ex02 = 2;
 	int *ptr1;
 	int **ptr2;
@@ -45,20 +52,28 @@ int   main(void)
 	ptr9 = &ptr8;
 
 	ft_ultimate_ft(ptr9);
-	printf("%d\n", num_ex02);
+
+	assert(num_ex02==42);
+
+	printf("All tests passed for ex01\n");
 
 
 	printf("%s\n", "==== 02 ft_swap ====");
+	printf("Tesing ex02\n");
+
 	int a = 42;
 	int b = 24;
 
-	printf("a: %d ; b: %d\n", a, b);
 	ft_swap(&a, &b);
-	printf("swapped\n");
-	printf("a: %d ; b: %d\n", a, b);
+
+	assert(a==24);
+	assert(b==42);
+
+	printf("All tests passed for ex02\n");
 
 
 	printf("%s\n", "==== 03 ft_div_mod ====");
+	printf("Tesing ex03\n");
 
 	int dem = 4242;
 	int nom = 424;
@@ -66,17 +81,24 @@ int   main(void)
 	int mod;
 
 	ft_div_mod(dem, nom, &div, &mod);
-	printf("dividing %d by %d\n", dem, nom);
-	printf("div: %d ; mod: %d\n", div, mod);
+
+	assert(div==10);
+	assert(mod==2);
+	printf("All tests passed for ex03\n");
 
 
 	printf("%s\n", "==== 04 ft_ultimate_div_mod ====");
+	printf("Tesing ex04\n");
+
 	int ultimate_dem = 4242;
 	int ultimate_nom = 424;
 
-	printf("dividing %d by %d\n", ultimate_dem, ultimate_nom);
 	ft_ultimate_div_mod(&ultimate_dem, &ultimate_nom);
-	printf("div: %d ; mod: %d\n", ultimate_dem, ultimate_nom);
+
+	assert(ultimate_dem==10);
+	assert(ultimate_nom==2);
+
+	printf("All tests passed for ex04\n");
 
 
 	printf("%s\n", "==== 05 ft_putstr ====");
@@ -86,31 +108,38 @@ int   main(void)
 
 
 	printf("%s\n", "==== 06 ft_putstr ====");
+	printf("Tesing ex06\n");
 	char *str_w_len;
 	str_w_len = "Life";
-	printf("Length: %d\n", ft_strlen(str_w_len));
+
+	assert(ft_strlen(str_w_len)==4);
+
+	printf("All tests passed for ex06\n");
 
 
 	printf("%s\n", "==== 07 ft_strrev ====");
 	// causes segment fault error when trying to run with other functions
 	char rev_str1[] = "";
-  	char rev_str2[] = "abcd12345";
+	char rev_str2[] = "abcd12345";
 
 	// printf("reversed: %s;\n", ft_strrev(rev_str1));
 	// printf("reversed: %s;\n", ft_strrev(rev_str2));
 
 
 	printf("%s\n", "==== 08 ft_atoi ====");
+	printf("Tesing ex08\n");
 
-	printf("%d\n",ft_atoi(" 98993489"));
-	printf("%d\n",ft_atoi("tutorialspoint.com"));
-	printf("%d\n",ft_atoi("a"));
-	printf("%d\n",ft_atoi("abc"));
-	printf("%d\n",ft_atoi(" -42"));
-	printf("%d\n",ft_atoi(" 14343 and abc"));
-	printf("%d\n",ft_atoi("-91283472332"));
-	printf("%d\n",ft_atoi("91283472332"));
-	printf("%d\n",ft_atoi("11111191283472332"));
+	assert(ft_atoi(" 98993489")==98993489);
+	assert(ft_atoi("tutorialspoint.com")==0);
+	assert(ft_atoi("a")==0);
+	assert(ft_atoi("abc")==0);
+	assert(ft_atoi(" -42")==-42);
+	assert(ft_atoi(" 14343 and abc")==14343);
+	assert(ft_atoi("-91283472332")==-2147483648);
+	assert(ft_atoi("91283472332")==2147483647);
+	assert(ft_atoi("11111191283472332")==2147483647);
+
+	printf("All tests passed for ex08\n");
 
 
 	printf("%s\n", "==== 09 ft_sort_integer_table ====");
