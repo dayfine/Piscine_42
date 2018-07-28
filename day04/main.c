ed4bc3a088eee05cd4fdf704b2835cc78e6fe884
dayfine <difancpa@gmail.com>
2018-07-27 19:22:45 -0700

    ex04-06

--- day04/main.c
+++ day04/main.c
@@ -17,7 +17,7 @@ int   main(void)
 {
 	printf("%s\n", "==== 00 ft_iterative_factorial ====");
 	printf("Tesing ex00\n");
-    assert(ft_iterative_factorial(0)==1);
+	assert(ft_iterative_factorial(0)==1);
 	assert(ft_iterative_factorial(1)==1);
 	assert(ft_iterative_factorial(2)==2);
 	assert(ft_iterative_factorial(3)==6);
@@ -25,12 +25,12 @@ int   main(void)
 	assert(ft_iterative_factorial(5)==120);
 	assert(ft_iterative_factorial(-1)==0);
 	assert(ft_iterative_factorial(13)==0);
-    printf("All tests passed for ex00\n");
+	printf("All tests passed for ex00\n");
 
 
 	printf("%s\n", "==== 01 ft_recursive_factorial ====");
 	printf("Tesing ex01\n");
-    assert(ft_recursive_factorial(0)==1);
+	assert(ft_recursive_factorial(0)==1);
 	assert(ft_recursive_factorial(1)==1);
 	assert(ft_recursive_factorial(2)==2);
 	assert(ft_recursive_factorial(3)==6);
@@ -38,12 +38,12 @@ int   main(void)
 	assert(ft_recursive_factorial(5)==120);
 	assert(ft_recursive_factorial(-1)==0);
 	assert(ft_recursive_factorial(13)==0);
-    printf("All tests passed for ex01\n");
+	printf("All tests passed for ex01\n");
 
 
 	printf("%s\n", "==== 02 ft_iterative_power ====");
 	printf("Tesing ex02\n");
-    assert(ft_iterative_power(0, 0)==1);
+	assert(ft_iterative_power(0, 0)==1);
 	assert(ft_iterative_power(1, 0)==1);
 	assert(ft_iterative_power(0, 1)==0);
 	assert(ft_iterative_power(0, -1)==0);
@@ -53,12 +53,12 @@ int   main(void)
 	assert(ft_iterative_power(2, 10)==1024);
 	assert(ft_iterative_power(5, 2)==25);
 	assert(ft_iterative_power(7, 3)==343);
-    printf("All tests passed for ex02\n");
+	printf("All tests passed for ex02\n");
 
 
 	printf("%s\n", "==== 03 ft_recursive_power ====");
 	printf("Tesing ex03\n");
-    assert(ft_recursive_power(0, 0)==1);
+	assert(ft_recursive_power(0, 0)==1);
 	assert(ft_recursive_power(1, 0)==1);
 	assert(ft_recursive_power(0, 1)==0);
 	assert(ft_recursive_power(0, -1)==0);
@@ -68,16 +68,51 @@ int   main(void)
 	assert(ft_recursive_power(2, 10)==1024);
 	assert(ft_recursive_power(5, 2)==25);
 	assert(ft_recursive_power(7, 3)==343);
-    printf("All tests passed for ex03\n");
+	printf("All tests passed for ex03\n");
 
 
 	printf("%s\n", "==== 04 ft_fibonacci ====");
+	printf("Tesing ex04\n");
+	assert(ft_fibonacci(-2)==0);
+	assert(ft_fibonacci(-1)==0);
+	assert(ft_fibonacci(0)==0);
+	assert(ft_fibonacci(1)==1);
+	assert(ft_fibonacci(2)==1);
+	assert(ft_fibonacci(3)==2);
+	assert(ft_fibonacci(4)==3);
+	assert(ft_fibonacci(5)==5);
+	assert(ft_fibonacci(6)==8);
+	printf("All tests passed for ex04\n");
 
 
 	printf("%s\n", "==== 05 ft_sqrt ====");
+	printf("Tesing ex05\n");
+	assert(ft_sqrt(-2)==0);
+	assert(ft_sqrt(-1)==0);
+	assert(ft_sqrt(0)==0);
+	assert(ft_sqrt(1)==1);
+	assert(ft_sqrt(2)==0);
+	assert(ft_sqrt(3)==0);
+	assert(ft_sqrt(4)==2);
+	assert(ft_sqrt(9)==3);
+	assert(ft_sqrt(16)==4);
+	printf("All tests passed for ex05\n");
 
 
 	printf("%s\n", "==== 06 ft_is_prime ====");
+	printf("Tesing ex06\n");
+	assert(ft_is_prime(-2)==0);
+	assert(ft_is_prime(-1)==0);
+	assert(ft_is_prime(0)==0);
+	assert(ft_is_prime(1)==0);
+	assert(ft_is_prime(2)==1);
+	assert(ft_is_prime(3)==1);
+	assert(ft_is_prime(4)==0);
+	assert(ft_is_prime(9)==0);
+	assert(ft_is_prime(13)==1);
+	assert(ft_is_prime(17)==1);
+	assert(ft_is_prime(21)==0);
+	printf("All tests passed for ex06\n");
 
 
 	printf("%s\n", "==== 07 ft_find_next_prime ====");
