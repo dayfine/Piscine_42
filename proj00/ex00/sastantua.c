5c5e49896efa57acef1df7946f313f50174f60d9
difan <difancpa@gmail.com>
2018-07-28 20:56:21 -0700

    normalized sastantua

--- proj00/ex00/sastantua.c
+++ proj00/ex00/sastantua.c
@@ -1,12 +1,18 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sastantua.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/28 20:45:17 by dfan              #+#    #+#             */
+/*   Updated: 2018/07/28 20:47:44 by dfan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return 0;
-}
+int		ft_putchar(char c);
 
-void ft_put_line(char line[], int length)
+void	ft_put_line(char line[], int length)
 {
 	int idx;
 
@@ -19,67 +25,58 @@ void ft_put_line(char line[], int length)
 	ft_putchar('\n');
 }
 
-int get_max_width(int size)
+int		get_max_width(int size)
 {
 	int i;
 	int w;
 
 	i = 0;
 	w = 0;
-
 	while (i < size)
 	{
-		if (i == 0) {
+		if (i == 0)
+		{
 			w += 7;
-		} else {
+		}
+		else
+		{
 			w += (i + 5) * 2 + (i - 1) / 2 * 2;
 		}
 		i++;
 	}
-	return w;
+	return (w);
 }
 
-int get_num_lvs(int section)
-{
-	return (section + 3);
-}
-
-int get_sec_level_width(int sec, int level)
-{
-	return get_max_width(sec + 1) - (get_num_lvs(sec) - level - 1) * 2;
-}
-
-void put_door_on_line(int lv, int sec, char line[], int max_width)
+void	put_door_on_line(int lv, int sec, char line[], int max_width)
 {
 	int door_dim;
 	int side_fill;
 	int i;
 	int is_knob;
 
-	door_dim = sec % 2 == 0 ? sec + 1: sec;
-	if (lv > get_num_lvs(sec) - 1 - door_dim)
+	door_dim = sec % 2 == 0 ? sec + 1 : sec;
+	if (lv > sec + 3 - 1 - door_dim)
 	{
 		side_fill = (max_width - door_dim) / 2;
 		i = side_fill;
 		while (i < side_fill + door_dim)
 		{
 			is_knob = (sec >= 4
-						&& lv == get_num_lvs(sec) - 1 - door_dim / 2
+						&& lv == sec + 3 - 1 - door_dim / 2
 						&& i == side_fill + door_dim - 2);
-
 			line[i++] = is_knob ? '$' : '|';
 		}
 	}
 }
 
-void build_normal_level(int lv, int sec, int max_width, int build_door)
+void	build_normal_level(int lv, int sec, int max_width, int build_door)
 {
-	int side_fill;
-	int i;
-	char line[max_width];
+	int		side_fill;
+	int		i;
+	char	line[max_width];
 
 	i = 0;
-	side_fill = (max_width - get_sec_level_width(sec, lv)) / 2;
+	side_fill = (max_width - (get_max_width(sec + 1) - (sec - lv + 2) * 2)) / 2;
 	while (i < side_fill)
 	{
 		line[i++] = ' ';
@@ -101,7 +98,7 @@ void build_normal_level(int lv, int sec, int max_width, int build_door)
 	ft_put_line(line, max_width);
 }
 
-void sastantua(int size)
+void	sastantua(int size)
 {
 	int max_width;
 	int sec;
@@ -112,7 +109,7 @@ void sastantua(int size)
 	max_width = get_max_width(size);
 	while (sec < size)
 	{
-		levels = get_num_lvs(sec);
+		levels = sec + 3;
 		lv = 0;
 		while (lv < levels)
 		{
--- proj00/main.c
+++ proj00/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 
+#include "./ft_putchar.c"
 #include "./ex00/sastantua.c"
 
 
@@ -15,17 +16,6 @@ int main(void) {
 	assert(get_max_width(6)==95);
 	assert(get_max_width(7)==121);
 	assert(get_max_width(8)==151);
-	printf("Finished checking get_max_width\n");
-	printf("Checking get_level_width\n");
-	assert(get_sec_level_width(0, 0)==3);
-	assert(get_sec_level_width(0, 1)==5);
-	assert(get_sec_level_width(0, 2)==7);
-	assert(get_sec_level_width(1, 0)==13);
-	assert(get_sec_level_width(1, 1)==15);
-	assert(get_sec_level_width(1, 2)==17);
-	assert(get_sec_level_width(1, 3)==19);
-	assert(get_sec_level_width(6, 8)==121);
-	assert(get_sec_level_width(7, 9)==151);
 	printf("Finished checking get_max_width\n");
 	printf("=====================\n");
 	sastantua(0);
