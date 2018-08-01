f425a8945111fef2f2636d4e23bc0869c433136b
difan <difancpa@gmail.com>
2018-08-01 12:45:27 -0700

    getting started with day07

---
+++ day07/ex00/ft_strdup.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/01 12:27:44 by dfan              #+#    #+#             */
+/*   Updated: 2018/08/01 12:28:29 by dfan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strcpy_helper(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char *dest;
+	dest = malloc(sizeof(src) + 1);
+
+	ft_strcpy_helper(dest, src);
+	return dest;
+}
