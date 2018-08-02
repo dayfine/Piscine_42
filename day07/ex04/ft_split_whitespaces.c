0108f4b28f9143ac353529d040a2ad511de488fc
difan <difancpa@gmail.com>
2018-08-02 15:45:45 -0700

    some changes

--- day07/ex04/ft_split_whitespaces.c
+++ day07/ex04/ft_split_whitespaces.c
@@ -38,26 +38,24 @@ int		get_word_count(char *str, char *breaks)
 	while (str[i + 1] != '\0')
 	{
 		if (i == 0 && !includes(breaks, str[i]))
-		{
 			wc++;
-		}
 		else if (includes(breaks, str[i]) && !includes(breaks, str[i + 1]))
-		{
 			wc++;
-		}
 		i++;
 	}
 	return (wc);
 }
 
-void	build_word_helper(char *dest, int word_len, int idx, char *src)
+int		get_word_len(char **str_ptr, char *breaks)
 {
-	int j;
+	int i;
 
-	j = 0;
-	while (j < word_len)
-		dest[j++] = src[idx++];
-	dest[j] = '\0';
+	i = 0;
+	while (**str_ptr && includes(breaks, **str_ptr))
+		(*str_ptr)++;
+	while (*(*str_ptr + i) && !includes(breaks, *(*str_ptr + i)))
+		i++;
+	return (i);
 }
 
 char	**ft_split(char *str, char *breaks)
@@ -67,22 +65,18 @@ char	**ft_split(char *str, char *breaks)
 	int		word_len;
 	char	**dest;
 
-	dest = malloc(sizeof(char*) * (get_word_count(str, breaks) + 1));
-	i = 0;
+	dest = (char**)malloc(sizeof(char*) * (get_word_count(str, breaks) + 1));
 	k = 0;
-	while (str[i] != '\0')
+	while (k < get_word_count(str, breaks))
 	{
-		while (includes(breaks, str[i++]))
-			;
-		word_len = 0;
-		i -= i != 0;
-		while (str[i + word_len] && !includes(breaks, str[i + word_len]))
-			word_len++;
+		word_len = get_word_len(&str, breaks);
 		if (word_len)
 		{
-			dest[k] = malloc((word_len + 1) * sizeof(char));
-			build_word_helper(dest[k++], word_len, i, str);
-			i += word_len;
+			dest[k] = (char*)malloc((word_len + 1) * sizeof(char));
+			i = 0;
+			while (i < word_len)
+				dest[k][i++] = *str++;
+			dest[k++][i] = '\0';
 		}
 		else
 			dest[k] = 0;
--- day07/ex07/ft_split.c
+++ day07/ex07/ft_split.c
@@ -20,9 +20,7 @@ int		includes(char *str, char letter)
 	while (str[i] != '\0')
 	{
 		if (str[i++] == letter)
-		{
 			return (1);
-		}
 	}
 	return (0);
 }
@@ -35,22 +33,28 @@ int		get_word_count(char *str, char *breaks)
 	i = 0;
 	wc = 0;
 	if (str[0] == '\0')
-	{
 		return (0);
-	}
 	while (str[i + 1] != '\0')
 	{
 		if (i == 0 && !includes(breaks, str[i]))
-		{
 			wc++;
-		}
 		else if (includes(breaks, str[i]) && !includes(breaks, str[i + 1]))
-		{
 			wc++;
-		}
 		i++;
 	}
 	return (wc);
+}
+
+int		get_word_len(char *str, char *breaks)
+{
+	int i;
+
+	i = 0;
+	while (str && includes(breaks, *str))
+		str++;
+	while (str && !includes(breaks, *str++))
+		i++;
+	return (i);
 }
 
 void	build_word_helper(char *dest, int word_len, int idx, char *src)
@@ -70,12 +74,12 @@ char	**ft_split(char *str, char *breaks)
 	int		word_len;
 	char	**dest;
 
-	dest = malloc(sizeof(char*) * (get_word_count(str, breaks) + 1));
+	dest = (char**)malloc(sizeof(char*) * (get_word_count(str, breaks) + 1));
 	i = 0;
 	k = 0;
 	while (str[i] != '\0')
 	{
-		while (includes(breaks, str[i++]))
+		while (includes(breaks, *str++))
 			;
 		word_len = 0;
 		i -= i != 0;
@@ -83,7 +87,7 @@ char	**ft_split(char *str, char *breaks)
 			word_len++;
 		if (word_len)
 		{
-			dest[k] = malloc((word_len + 1) * sizeof(char));
+			dest[k] = (char*)malloc((word_len + 1) * sizeof(char));
 			build_word_helper(dest[k++], word_len, i, str);
 			i += word_len;
 		}
