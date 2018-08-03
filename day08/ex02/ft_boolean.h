0fa578e731337b151010a49cd0d5deaa78ecc75f
difan <difancpa@gmail.com>
2018-08-02 19:54:46 -0700

    initial normalization

--- day08/ex02/ft_boolean.h
+++ day08/ex02/ft_boolean.h
@@ -1,8 +1,26 @@
-#include <unistd.h>
-#define TRUE 1
-#define FALSE 0
-#define EVEN_MSG "I have an even number of arguments."
-#define ODD_MSG "I have an odd number of arguments."
-#define SUCCESS 0
-#define EVEN(x) (1 - (x % 2))
-typedef int t_bool;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/02 19:35:40 by dfan              #+#    #+#             */
+/*   Updated: 2018/08/02 19:35:43 by dfan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments."
+# define ODD_MSG "I have an odd number of arguments."
+# define SUCCESS 0
+# define EVEN(x) (1 - (x % 2))
+
+typedef int	t_bool;
+
+#endif
