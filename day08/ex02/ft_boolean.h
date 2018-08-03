5da0b525604124dff00185b9bc81898e42928c34
difan <difancpa@gmail.com>
2018-08-02 19:24:27 -0700

    getting started with day08

---
+++ day08/ex02/ft_boolean.h
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#define TRUE 1
+#define FALSE 0
+#define EVEN_MSG "I have an even number of arguments."
+#define ODD_MSG "I have an odd number of arguments."
+#define SUCCESS 0
+#define EVEN(x) (1 - (x % 2))
+typedef int t_bool;
