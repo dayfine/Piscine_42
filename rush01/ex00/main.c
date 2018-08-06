c4cce8e3a8a49d30b06c99ed8747e67d3c143a4c
difan <difancpa@gmail.com>
2018-08-05 21:46:08 -0700

    a little error checking

--- rush01/ex00/main.c
+++ rush01/ex00/main.c
@@ -14,18 +14,49 @@
 #include "ft_sudoku_board.h"
 #include "ft_sudoku_backtracking.h"
 
-int		main(int argc, char **argv)
+int		input_is_invalid(int argc, char **argv)
 {
-	t_board *board;
+	int i;
+	int j;
 
 	if (argc != 10)
+		return (1);
+	i = 0;
+	while (++i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+			j++;
+		if (j != 9)
+			return (1);
+	}
+	return (0);
+}
+
+int		main(int argc, char **argv)
+{
+	int		result;
+	t_board *board;
+
+	if (input_is_invalid(argc, argv))
 	{
 		display_error();
 		return (1);
 	}
-	argv++;
-	board = create_board(argv);
-	backtrack(board);
-	print_board(board);
-	return (0);
+	else
+	{
+		argv++;
+		board = create_board(argv);
+		result = backtrack(board);
+		if (result)
+		{
+			print_board(board);
+			return (0);
+		}
+		else
+		{
+			display_error();
+			return (1);
+		}
+	}
 }
