a2df5251882337326a7c087bcd95c9581e2ebd8a
difan <difancpa@gmail.com>
2018-08-08 15:19:26 -0700

    d12ex00

--- day12/ex00/ft_display_file.c
+++ day12/ex00/ft_display_file.c
@@ -10,4 +10,47 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <fcntl.h>
 
+#define BUF_SIZE 4096
+
+void	ft_putstr(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	write(1, str, len);
+}
+
+void	ft_putline(char *str)
+{
+	ft_putstr(str);
+	write(1, "\n", 1);
+}
+
+int		main(int argc, char *argv[])
+{
+	int		fd;
+	int		ret;
+	char	buf[BUF_SIZE + 1];
+
+	if (argc != 2)
+	{
+		ft_putline(argc == 1 ? "File name missing." : "Too many arguments.");
+		return (1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (1);
+	while ((ret = read(fd, buf, BUF_SIZE)))
+	{
+		buf[ret] = '\0';
+		ft_putstr(buf);
+	}
+	if (close(fd) == -1)
+		return (1);
+	return (0);
+}
